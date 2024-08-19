2758f18ba9282d299df2489951d0bb32e6d7d545
Jim Nguyen <jimnguyen77@gmail.com>
2024-08-18 17:35:15 -0700

    updated script files, make it simpler

--- setup.sh
+++ setup.sh
@@ -5,12 +5,11 @@ SOURCE_DIR="$(pwd)"
 DEST_DIR="/usr/local/bin"
 
 # Move the scripts to /usr/local/bin
-echo "Moving scripts to $DEST_DIR..."
-sudo cp "$SOURCE_DIR/mark-junk-read.sh" "$DEST_DIR/mark-junk-read.sh"
+echo "Copying AppleScript to $DEST_DIR..."
 sudo cp "$SOURCE_DIR/mark-junk-read.scpt" "$DEST_DIR/mark-junk-read.scpt"
 
 # Add the cron job
 echo "Setting up the cron job..."
-(crontab -l 2>/dev/null; echo "0 * * * * $DEST_DIR/mark-junk-read.sh") | crontab -
+(crontab -l 2>/dev/null; echo "0 * * * * osascript $DEST_DIR/mark-junk-read.scpt") | crontab -
 
 echo "Setup complete!"
